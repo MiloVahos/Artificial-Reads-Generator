b636fec9efed81714f3364b3fb660bdf5256e7c6
Raykgeneer96 <juancamilopv@hotmail.com>
2018-07-09 07:43:18 -0500

    Modificación del archivo para agregar el primer ciclo

--- readsGen.c
+++ readsGen.c
@@ -1,14 +1,11 @@
 /*
  *  @Developer:     Juan Camilo Peña Vahos
- *  @Last Revised:  08/07/2018
+ *  @Last Revised:  09/07/2018
  *  @Description:   Generador artificial de reads para simular casos de prueba del compresor.
  *
 */
 /*
  *	Todo: 	     	Inicializar Read usando memoria dinámica
-*/
-/*
- *	ULTIMA LABOR:		 
 */
 /*								ESTRUCTURA DE UN READ
  * LINEA 1: (IDENTIFICADOR) Siempre empieza con @
@@ -41,45 +38,62 @@
  *		A. GENERAR ALEATORIAMENTE LA POSICIÓN DE MAPEO
 		B. CALCULAR EL MATCHING
 */
-	
-
+/*
+ * 	SALIDA DEL PROGRAMA:
+ * 		1. ARCHIVO .fastq CON BXC READS
+ * 		2. ARCHIVO .fastqseq CON LAS SECUENCIAS DE CADA READ,UNA POR LÍNEA
+ * 		3. ARCHIVO .aling CONTIENE POR READ LOS DATOS DEL MATCHING DEL READ Y TODOS LOS DATOS
+ * 						  DE LOS ERRORES
+ * 		4. ARCHIVO .meta CONTIENE LOS METADATOS DEL EXPERIMENTO,LOS QUE SE PASAN POR LA CONSOLA
+ * 					     Y ALGUNOS QUE SE GENERAN EN EL TRANSCURSO DE LA EJECUCACIÓN COMO LOS
+ * 						 NOMBRES DE LOS ARCHIVOS ANTERIORES
+*/ 
 
 //LIBRERÍAS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
+#include <inttypes.h>
 
 //DECLARACIÓN DE CONSTANTES
 #define MATCHING_TYPES 4
+#define EXP 2.71828
 
 //DECLARACIÓN DE FUNCIONES
-void ReverseRead(char*,long);
-void ComplementRead(char*,long);
-int  BusqBin_Rul(double[], int, double);
-double CalculaTotal(int,double[]);
-double LanzarDado();
+void 	ReverseRead(char*,long);
+void 	ComplementRead(char*,long);
+int  	BusqBin_Rul(double[], int, double);
+double 	CalculaTotal(int,double[]);
+double 	LanzarDado();
+long int contChars(char*);
+void 	getReference(char*,char*);
+void 	getRead(char*,char*,long int,int);
 
 //DECLARACIÓN DE VARIABLES GLOBALES
 
 int main (int argc, char *argv[]) {	
     
 	//ARGUMENTOS DE ENTRADA
-	char* DATA;
-	char* I;
-	char* Q;
-	int L	=	0;
-	int	C	=	0;
-	int B	=	0;
-	int E	=	0;
+	char 	*DATA;
+	char 	*I;
+	char 	*Q;
+	int 	L	=	0;
+	int		C	=	0;
+	int 	B	=	0;
+	int 	E	=	0;
 
 	//VARIABLES DEL PROCESO PARA SALIDA
-	float lambda = 0;									//
+	float 	lambda = 0;									//
 	char	*MT;										//MATCHING TYPE
 	char	*OT;										//OPERATION (MUTATION) TYPE
 
 	//VARIABLES DEL PROCESO PARA OPERAR
-	char	*Reference	=	NULL;						//REFERENCIA OBTENIDA DEL ARCHIVO FASTA
+	char		*Reference	=	NULL;					//REFERENCIA OBTENIDA DEL ARCHIVO FASTA
+	char 		*Name		=	NULL;					//NOMBRE DE LOS ARCHIVOS
+	int			TotalReads;								//TotalReads	=	BxC
+	long int 	TotalChars;								//Total de caracteres en la referencia
 
 	//Obtener los datos suministrados en la linea de comando
 	if(argc>1){
@@ -100,28 +114,119 @@ int main (int argc, char *argv[]) {
 				E	=	atoi(argv[i+1]);
 		}
 	}
+
+	//OBTENER EL NOMBRE DE LA SECUENCIA Y CREAR EL NOMBRE DE LOS ARCHIVOS DE SALIDA
+	Name	=	malloc(50*sizeof(char));
+	char *dot;
+	dot		=	strrchr(DATA,'.');
+	strncpy(Name,DATA,dot - DATA);
+
 	//A. GENERAR ALEATORIAMENTE LA POSICIÓN DE MAPEO
     //	1.SABER LA CANTIDAD DE CARACTERES DE LA REFERENCIA
-	int contChars	=	0;							
+	TotalChars	=	contChars(DATA);
+	//		2.OBTENER LA REFERENCIA
+	Reference	=	malloc(TotalChars*sizeof(char*));
+	getReference(DATA,Reference);
+	TotalReads	=	B*C;				//Número total de Reads a generar
+
+	for(int ReadsCicle = 0;	ReadsCicle<TotalReads; ReadsCicle++){
+		//			3.GENERAR UNA POSICIÓN ALEATORIA EN EL RANGO [0,LengthRef-LengthRead]
+		srand(time(0));
+		long int position = (rand() %((TotalChars-L) - 0 + 1)) + 0;
+		//				4.SACAR LA PORCIÓN DEL ARREGLO
+		char *Read	=	NULL;
+		Read	=	malloc(L*sizeof(char*));
+		getRead(Reference,Read,position,L);
+		printf("Read:	%s\n",Read);
+		/*//B.CALCULAR EL MATCHING
+		
+		//ORDEN DE LOS ARREGLOS 
+		//FORWARD(F), REVERSE(R), COMPLEMENT(C), REVERSE COMPLEMENT(E)
+		double MatTypeStats[MATCHING_TYPES]   =   {0.4,0.4,0.1,0.1};  
+		double Acum_fun[MATCHING_TYPES];
+
+		double total_adapt = CalculaTotal(MATCHING_TYPES,MatTypeStats);
+		Acum_fun[0]= MatTypeStats[0] / total_adapt;
+		for (int i=1; i<MATCHING_TYPES; i++){
+			Acum_fun[i]= (MatTypeStats[i] / total_adapt) + Acum_fun[i-1];
+		}
+
+		//LANZAR EL DADO
+		double dado = LanzarDado();
+		printf("Resultado del dado = %lf\n", dado);
+
+		//GIRAR LA RULETA
+		int MatTypeSel  =   BusqBin_Rul(Acum_fun,MATCHING_TYPES,dado);
+		printf("Selección de la ruleta = %d\n", MatTypeSel);
+
+		switch(MatTypeSel){
+			case 0:                     //FORWARD MATCH
+				MT  =   "F";
+				printf("F: %s\n",READ);
+			break;
+			case 1:                     //REVERSE MATCH
+				MT  =   "R";
+				ReverseRead(READ,L);
+				printf("R: %s\n",READ);
+			break;
+			case 2:                     //COMPLEMENT MATCH
+				MT  =   "C";
+				ComplementRead(READ,L);
+				printf("C: %s\n",READ);
+			break;
+			case 3:                     //REVERSE COMPLEMENT MATCH
+				MT  =   "E";
+				ComplementRead(READ,L);
+				ReverseRead(READ,L);
+				printf("E: %s\n",READ);                
+			break;
+			default:    printf ("**Error in the matching selection, wrong input base**");
+		}*/
+
+		free(Read);
+	}
+
+    
+
+
+	free(Name);
+	free(Reference);
+	return 0;
+}
+
+//FUNCIONES
+
+//contChars: Cuenta el número de caractéres que tiene el archivo de referencia
+//@param: char *FileName : Nombre del archivo
+long int contChars(char *FileName){
+	long int cont	=	0;							
     int estado;
 	char c;
 	FILE *pf;
 
-	pf = fopen(DATA,"r"); 
+	pf = fopen(FileName,"r"); 
 	if(pf!=NULL){								
 		while((c = getc(pf)) != EOF){
             if(c == '>')    estado  =   0;		//EL ESTADO 0 SON LOS COMENTARIOS           
             switch(estado){
                 case 0: if(c    ==  '\n')   estado  =   1;  break;
-                case 1: if(c    !=  '\n')   contChars++;    break;
+                case 1: if(c    !=  '\n')   cont++;    break;
             }
 		}
 	}
-	fclose(pf);
-	Reference	=	malloc(contChars*sizeof(char*));
-	//		2.OBTENER LA REFERENCIA
+
+	return cont;
+}
+
+//getReference: Obtiene la referencia de los archivos FASTA y la guarda en un arreglo
+//@param: char *FileName : Nombre del archivo
+//@param: char *Reference: Arreglo en el que se guarda la referencia
+void getReference(char *FileName, char *Reference){
+	int estado;
+	char c;
 	int j	=	0;
-	pf	=	fopen(DATA,"r");
+	FILE *pf;
+	pf	=	fopen(FileName,"r");
 	if(pf	!=	NULL){
 		while((c = getc(pf)) != EOF){
             if(c == '>')    estado  =   0;                          
@@ -136,12 +241,14 @@ int main (int argc, char *argv[]) {
 		}
 	}
 	fclose(pf);
-    //			3.GENERAR UNA POSICIÓN ALEATORIA EN EL RANGO [0,LengthRef-LengthRead]
-    srand(time(0));
-    int position = (rand() %((contChars-L) - 0 + 1)) + 0;
-    //				4.SACAR LA PORCIÓN DEL ARREGLO
-	printf("L	=	%d;  POS	=	%d\n",L,position);
-	char READ[L];
+}
+
+//getRead:	Obtiene el read de la referencia
+//@param:	char *Reference		:Arreglo con la referencia
+//@param:	char *READ			:Arreglo donde se va a almacenar el Read
+//@param:	long int position	:Posición desde la cual se va a tomar el Read
+//@param:	int L				:Longitud del Read
+void getRead(char* Reference,char* READ,long int position,int L){
 	for(int i = 0;	i < L;	i++){
 		switch(Reference[position+i]){
 			case 'a':	READ[i] = 'A'; break;
@@ -155,57 +262,11 @@ int main (int argc, char *argv[]) {
 			default:	READ[i]	= 'N';
 		}
 	}
-	printf("Read:	%s\n",READ);
-	//B.CALCULAR EL MATCHING
-	
-	//ORDEN DE LOS ARREGLOS 
-    //FORWARD(F), REVERSE(R), COMPLEMENT(C), REVERSE COMPLEMENT(E)
-    double MatTypeStats[MATCHING_TYPES]   =   {0.4,0.4,0.1,0.1};  
-    double Acum_fun[MATCHING_TYPES];
-
-	double total_adapt = CalculaTotal(MATCHING_TYPES,MatTypeStats);
-	Acum_fun[0]= MatTypeStats[0] / total_adapt;
-	for (int i=1; i<MATCHING_TYPES; i++){
-        Acum_fun[i]= (MatTypeStats[i] / total_adapt) + Acum_fun[i-1];
-    }
-
-    //LANZAR EL DADO
-    double dado = LanzarDado();
-    printf("Resultado del dado = %lf\n", dado);
-
-    //GIRAR LA RULETA
-    int MatTypeSel  =   BusqBin_Rul(Acum_fun,MATCHING_TYPES,dado);
-    printf("Selección de la ruleta = %d\n", MatTypeSel);
-
-    switch(MatTypeSel){
-        case 0:                     //FORWARD MATCH
-            MT  =   "F";
-            printf("F: %s\n",READ);
-        break;
-        case 1:                     //REVERSE MATCH
-            MT  =   "R";
-            ReverseRead(READ,L);
-            printf("R: %s\n",READ);
-        break;
-        case 2:                     //COMPLEMENT MATCH
-            MT  =   "C";
-            ComplementRead(READ,L);
-            printf("C: %s\n",READ);
-        break;
-        case 3:                     //REVERSE COMPLEMENT MATCH
-            MT  =   "E";
-            ComplementRead(READ,L);
-            ReverseRead(READ,L);
-            printf("E: %s\n",READ);                
-        break;
-        default:    printf ("**Error in the matching selection, wrong input base**");
-    }
-
-	return 0;
 }
 
-
-//Implementa el Inversor de reads 
+//ReverseRead:	Implementa el inversor de reads
+//@param:	char *Read	:	Arreglo con el Read
+//@param:	long length	:	Longitud del Read
 void ReverseRead(char *Read, long length){
 	char aux;
     int resto = length%2;
@@ -217,7 +278,9 @@ void ReverseRead(char *Read, long length){
 	}
 }
 
-//Implementa el complementador de reads (T,A)(C,G)
+//ComplementRead:	Implementa el complementador de reads (T,A)(C,G)
+//@param:	char *Read	:	Arreglo con el Read
+//@param:	long length	:	Longitud del Read
 void ComplementRead(char *Read, long length){
 	char Compl;
 	int i;
