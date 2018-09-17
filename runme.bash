c9f1144658aaabc2be68238b6ff16fa01a132b31
Raykgeneer96 <juancamilopv@hotmail.com>
2018-09-17 10:23:11 -0500

    Generador de pruebas y hard reset

--- runme.bash
+++ runme.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for C in 10 20 30 40 50 60 70 80 90 100; do
+for C in 1 10 20 30 40 50 60 70 80 90 100; do
     mkdir "Prueba con C = $C"
-    ./ARF -DATA lambda_virus.fa -I -Q -L 1024 -B 20 -C $C -P0 0.02
-    mv "lambda_virus.fastq" "Prueba con C = $C" 
-    mv "lambda_virus.fastqseq" "Prueba con C = $C" 
-    mv "lambda_virus.meta" "Prueba con C = $C" 
-    mv "lambda_virus.align" "Prueba con C = $C" 
+    ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B 20 -C $C -P0 0.3625
+    mv "GRCh38.fastq" "Prueba con C = $C" 
+    mv "GRCh38.fastqseq" "Prueba con C = $C" 
+    mv "GRCh38.meta" "Prueba con C = $C" 
+    mv "GRCh38.align" "Prueba con C = $C" 
 done
